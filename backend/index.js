aa8ae1c4514cd79b79ab2b71c02823fe59c82e34
Kreativepardha <saradhipardha12@gmail.com>
2025-07-21 23:24:44 +0530

    Feat: Added OpenAi

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,19 @@ import express from 'express'
 import cors from 'cors'
 import multer from 'multer'
 import { Queue } from 'bullmq'
+import { QdrantVectorStore } from '@langchain/qdrant'
+import { OpenAIEmbeddings } from '@langchain/openai'
+import { Worker } from 'bullmq'
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf'
+import { CharacterTextSplitter } from '@langchain/textsplitters'
+import dotenv from 'dotenv';
+dotenv.config();
+
+import OpenAI from 'openai'
+
+const client = new OpenAI({
+  apikey: process.env.OPENAI_API_KEY,
+})
 
 const queue = new Queue('pdf-processing', {
   connection: {
@@ -9,6 +22,11 @@ const queue = new Queue('pdf-processing', {
     port: 6379 // Assuming Redis is running on localhost:6379
   }
 })
+
+// const vectorStore = QdrantVectorStore.fromExistingCollection({
+//   url: 'http://localhost:6333',
+//   collectionName: 'pdf-rag',
+// })
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -48,6 +66,46 @@ app.post('/api/upload', upload.single('pdf'), async (req, res) => {
   })
 }
 )
+
+app.get('/chat', async (req, res) => {
+  const userQuery = 'What is in skill section '
+  const embeddings = new OpenAIEmbeddings({
+    apikey: process.env.OPENAI_API_KEY,
+    model: 'text-embedding-3-small',
+  })
+  const vectorStore = await QdrantVectorStore.fromExistingCollection(
+    embeddings,
+    {
+      url: 'http://localhost:6333',
+      collectionName: 'pdf-rag',
+    }
+  )
+  const ret = vectorStore.asRetriever({
+    k: 2,
+  })
+
+  const result = await ret.invoke(userQuery)
+
+  const SYSTEM_PROMPT = `You are a helpful assistant who answers the user query based on available context from PDF files.
+  context: ${JSON.stringify(result)}
+ If you don't know the answer, just say "I don't know".` 
+
+ const chatResult = await client.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'system', content: SYSTEM_PROMPT },
+      { role: 'user', content: userQuery }
+    ],
+  })
+
+
+  return res.json({
+    status: 'Success',
+    message: 'Query processed successfully',
+    result: chatResult.choices[0].message.content,
+    docs: result
+  })
+})
 
 app.listen(8000, () => {
   console.log('Server is running on http://localhost:8000')
